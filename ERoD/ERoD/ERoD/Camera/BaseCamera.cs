def956bc7002e99be487a26e6654322a51802c05
Johan Bowald <johan.bowald@gmail.com>
2014-02-17 16:41:33 +0100

    A basic Camera Class has been implemented

--- ERoD/ERoD/ERoD/Camera/BaseCamera.cs
+++ ERoD/ERoD/ERoD/Camera/BaseCamera.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,51 +9,77 @@ namespace ERoD
 {
     class BaseCamera : GameComponent, ICamera
     {
+        protected Matrix view;
+        protected Vector3 position;
+        protected Matrix projection;
+        protected Quaternion rotation;
+        protected Matrix world;
+        protected Viewport viewport;
+        protected float nearPlane;
+        protected float farPlane;
 
         Matrix ICamera.View
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get{ return view; }
+            set{ view = value; }
         }
 
         Matrix ICamera.Projection
         {
-            get { throw new NotImplementedException(); }
+            get { return projection; }
         }
 
         Vector3 ICamera.Position
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return position; }
+            set { position = value; }
         }
 
         Quaternion ICamera.Rotation
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get{ return rotation; }
+            set{ rotation = value; }
         }
 
         Matrix ICamera.World
         {
-            get { throw new NotImplementedException(); }
+            get { return world; }
+        }
+
+        Viewport ICamera.Viewport
+        {
+            get { return viewport; }
+            set { viewport = value; }
+        }
+
+        public BaseCamera(Game game, float nearPlane, float farPlane) : base(game)
+        {
+            position = Vector3.Zero;
+            this.nearPlane = nearPlane;
+            this.farPlane = farPlane;
+
+            game.Components.Add(this);
+        }
+
+        public override void Initialize()
+        {
+            viewport = Game.GraphicsDevice.Viewport;
+            viewport.MinDepth = nearPlane;
+            viewport.MaxDepth = farPlane;
+        }
+        public override void Update(GameTime gameTime)
+        {
+            world = Matrix.CreateFromQuaternion(rotation) * Matrix.CreateTranslation(position);
+            view = Matrix.Invert(world);
+
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, viewport.AspectRatio, viewport.MinDepth, viewport.MaxDepth);
+
+            base.Update(gameTime);
+        }
+        public virtual void Dispose()
+        {
+            base.Dispose();
+            Game.Components.Remove(this);
         }
     }
 }
--- ERoD/ERoD/ERoD/Interface/ICamera.cs
+++ ERoD/ERoD/ERoD/Interface/ICamera.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,7 @@ namespace ERoD
         Vector3 Position { get; set; }
         Quaternion Rotation { get; set; }
         Matrix World { get; }
+        Viewport Viewport { get; set; }
+        void Dispose();
     }
 }
